5aac9a05215974274682e130e4e6426a861d5d8c
Brenden McEvoy <brenden.mcevoy24@gmail.com>
2022-06-15 14:49:30 -0600

    bad start

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -4,9 +4,25 @@ namespace Tic_Tac_Toe
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            TicTacToe game = new();
+            game.GetTable();
+
+            Console.WriteLine("Player One, you will be marker '1'.\nPlayer Two, you will be marker '2'.");
+            Console.WriteLine("Player One, input a coordinate to make a play. ex. '1,1 or 2,2");
+            var p1Move = int.Parse(Console.ReadLine());
+            
+
+            
+
+            
+
+            
+            
+
+           
         }
     }
 }
--- Tic-Tac-Toe/Tic-Tac-Toe/TicTacToe.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/TicTacToe.cs
@@ -10,29 +10,74 @@ namespace Tic_Tac_Toe
     {
         private int[,] game;
 
-        private string playerOne;
+        public string playerOne = "Player One";
+        public string playerTwo = "Player Two";
 
-        private string playerTwo;
 
 
         public TicTacToe()
         {
-            game = new int[,] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
-                              
+           game = new int[3, 3];
+
         }
 
-        public void BeginGame(string playerName, string player2Name)
+        public void GetTable() 
         {
-
+            Console.WriteLine($"{game[0, 0]}{game[0, 1]}{game[0, 2]}\n{game[1, 0]}{game[1, 1]}{game[1, 2]}\n{game[2, 0]}{game[2, 1]}{game[2, 2]}");
         }
-        public void MakeMove(string player, int location)
+            
+        public string DetermineWinner()
         {
+            for(int i = 0; i <= 9; i++)
+            {
+                int luckyNumber = 0;
+                switch (i)
+                {
+                    case 0: 
+                        luckyNumber = game[0, 0] + game[1, 1] + game[2, 2];
+                        break;
+                    case 1:                       
+                        luckyNumber = game[0, 2] + game[1, 1] + game[2, 0];
+                        break;
+                    case 2:
+                        luckyNumber = game[0, 0] + game[0, 1] + game[0, 2];
+                        break;
+                    case 3:
+                        luckyNumber = game[1, 0] + game[1, 1] + game[1, 2];
+                        break;
+                    case 4:
+                        luckyNumber = game[2, 0] + game[2, 1] + game[2, 2];
+                        break;
+                    case 5:
+                        luckyNumber = game[0, 0] + game[1, 0] + game[2, 0];
+                        break;
+                    case 6:
+                        luckyNumber = game[0, 1] + game[1, 1] + game[2, 1];
+                        break;
+                    case 7:
+                        luckyNumber = game[0, 2] + game[1, 2] + game[2, 2];
+                        break;                   
+                }
+
+                if (luckyNumber == 3)
+                {
+                    return playerOne;
+                }else if (luckyNumber == 6)
+                {
+                    return playerTwo;
+                }
+        
+            }
+
+            return null;
 
         }
 
-        public void DetermineWinner()
-        {
 
-        }
+
+
+
     }
+
+        
 }
